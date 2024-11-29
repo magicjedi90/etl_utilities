f4d7e29f28eaa744511937ca8e0d8122e1828a70
Eiji Dougal <eiji.dougal@gmail.com>
2024-11-29 10:58:39 -0700

    start bulk loader implementation

--- src/etl/database/bulk_loader.py
+++ src/etl/database/bulk_loader.py
@@ -1,14 +1,7 @@
 import pyodbc
 import pandas as pd
 
-class BulkLoader:
-    def __init__(self, connection: pyodbc.Connection, schema_name: str, table_name: str):
-        """
-        Initializes the BulkLoader object.
 
-        Args:
-            connection: The database connection object.
-            schema_name: The schema of the destination table.
-            table_name: The name of the destination table.
-        """
+class BulkLoader:
+    def __init__(self, connection: pyodbc.Connection):
         self.connection = connection
