13586d3cff5e05a8fa391c4f2985671a2799e736
Jesse <magicjedi90@aim.com>
2025-01-13 11:30:31 -0700

    Refactor table comparison logic to modularize column handling.
    Extract column name and data retrieval into standalone static methods, reducing redundancy and improving code readability. Updated similarity computation to account for set comparisons. Incremented version to `v0.10.2`.

--- src/etl/database/differentiator.py
+++ src/etl/database/differentiator.py
@@ -12,33 +12,12 @@ class Differentiator:
     def find_table_similarities(connection: PoolProxiedConnection, source_schema: str, target_schema: str,
                                 source_table: str,
                                 target_table: str, similarity_threshold: float = .8):
-        get_target_column_info_query = (
-            f'select COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION '
-            f'from INFORMATION_SCHEMA.columns '
-            f'where table_schema = \'{target_schema}\' and table_name = \'{target_table}\'')
-        target_column_info_df = pd.read_sql(get_target_column_info_query, connection)
-        get_source_column_info_query = (
-            f'select COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION '
-            f'from INFORMATION_SCHEMA.columns '
-            f'where table_schema = \'{source_schema}\' and table_name = \'{source_table}\'')
-        source_column_info_df = pd.read_sql(get_source_column_info_query, connection)
-        source_columns = source_column_info_df['COLUMN_NAME'].tolist()
-        target_columns = target_column_info_df['COLUMN_NAME'].tolist()
-        source_column_list = []
-        target_column_list = []
-        logger.info("generating source column list")
-        for column in source_columns:
-            query = f'select distinct ([{column}]) from {source_schema}.{source_table}'
-            column_series = pd.read_sql(query, connection).squeeze()
-            column_series = column_series.dropna()
-            column_dict = {"name": column, "data": column_series}
-            source_column_list.append(column_dict)
-        for column in target_columns:
-            query = f'select distinct ([{column}]) from {target_schema}.{target_table}'
-            column_series = pd.read_sql(query, connection).squeeze()
-            column_series = column_series.dropna()
-            column_dict = {"name": column, "data": column_series}
-            target_column_list.append(column_dict)
+        target_columns = Differentiator.get_column_name_list(connection, target_schema, target_table)
+        source_columns = Differentiator.get_column_name_list(connection, source_schema, source_table)
+        target_column_list = Differentiator.get_column_dict_list(connection, target_columns, target_schema,
+                                                                 target_table)
+        source_column_list = Differentiator.get_column_dict_list(connection, source_columns, source_schema,
+                                                                 source_table)
         similar_columns = []
         unique_source_columns = []
         non_unique_target_columns = []
@@ -49,7 +28,9 @@ class Differentiator:
                 if source_column['name'] == target_column['name']:
                     same_name_columns.append(source_column['name'])
                 try:
-                    similarity = (source_column['data'] == target_column['data']).mean()
+                    similarity_source = source_column['data'].isin(target_column['data'])
+                    similarity_target = target_column['data'].isin(source_column['data'])
+                    similarity = max(similarity_source, similarity_target)
                     if similarity >= similarity_threshold:
                         is_unique_source_column = False
                         column_dict = {
@@ -60,19 +41,22 @@ class Differentiator:
                         similar_columns.append(column_dict)
                         is_unique_source_column = False
                         non_unique_target_columns.append(target_column['name'])
-                except ValueError as e:
+                except (ValueError, TypeError) as e:
                     logger.debug(f'{source_column["name"]} and {target_column["name"]} are not comparable: {e}')
             if is_unique_source_column:
                 unique_source_columns.append(source_column['name'])
         unique_target_columns = []
         if non_unique_target_columns.__len__() < target_columns.__len__():
             unique_target_columns = [column for column in target_columns if column not in non_unique_target_columns]
+        same_name_columns.sort()
+        unique_source_columns.sort()
+        unique_target_columns.sort()
         message = (
             f'\n{"=" * 50}\ntable comparison between {source_table} and {target_table}\n'
             f'{"*" * 8} Columns with the same name:\n'
             f'{", ".join(same_name_columns)}\n'
             f'{"*" * 8} Columns with similar data:\n'
-            f'{similar_columns}\n'
+            f'{"\n".join([f'{column}' for column in similar_columns])}\n'
             f'{"*" * 8} Source Columns with unique data:\n'
             f'{", ".join(unique_source_columns)}\n'
             f'{"*" * 8} Target Columns with unique data:\n'
@@ -83,11 +67,32 @@ class Differentiator:
         return same_name_columns, similar_columns, unique_source_columns, unique_target_columns
 
     @staticmethod
+    def get_column_dict_list(connection, column_names, schema, table):
+        source_column_list = []
+        for column in column_names:
+            query = f'select distinct ([{column}]) from {schema}.{table}'
+            column_series = pd.read_sql(query, connection).squeeze()
+            column_series = column_series.dropna()
+            column_dict = {"name": column, "data": column_series}
+            source_column_list.append(column_dict)
+        return source_column_list
+
+    @staticmethod
+    def get_column_name_list(connection, schema, table):
+        get_target_column_info_query = (
+            f'select COLUMN_NAME '
+            f'from INFORMATION_SCHEMA.columns '
+            f'where table_schema = \'{schema}\' and table_name = \'{table}\'')
+        target_column_info_df = pd.read_sql(get_target_column_info_query, connection)
+        target_columns = target_column_info_df['COLUMN_NAME'].tolist()
+        return target_columns
+
+    @staticmethod
     def find_table_similarities_in_schema(connection: PoolProxiedConnection, schema: str,
                                           similarity_threshold: float = .8):
         get_table_info_query = (
             f'select TABLE_NAME from INFORMATION_SCHEMA.TABLES '
-            f'where TABLE_SCHEMA = \'{schema}\''
+            f'where TABLE_SCHEMA = \'{schema}\' and TABLE_TYPE = \'BASE TABLE\';'
         )
         table_info_df = pd.read_sql(get_table_info_query, connection)
         table_list = table_info_df['TABLE_NAME'].tolist()
@@ -119,6 +124,9 @@ class Differentiator:
                 source_column = f'{table_set[0]}.{column}'
                 if source_column not in unique_columns_list and source_column not in similar_columns_list:
                     unique_columns_list.append(source_column)
+        same_name_columns_list.sort()
+        similar_columns_list.sort()
+        unique_columns_list.sort()
         message = (
             f'\n{"=" * 50}\n{schema} schema table differences\n'
             f'{"*" * 8} Columns with the same name:\n'
