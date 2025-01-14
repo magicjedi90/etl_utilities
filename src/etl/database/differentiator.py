6701a6291d7f59fa3a9428e2878f90f7c92840b3
Jesse <magicjedi90@aim.com>
2025-01-14 08:42:23 -0700

    Refactor table differentiator logic for improved clarity
    Refactored column comparison logic in `differentiator.py` to enhance data structure usage and simplify data processing. Replaced list-based logic with DataFrame-based operations for better alignment and filtering. Updated version to `v0.10.3` to reflect these improvements.

--- src/etl/database/differentiator.py
+++ src/etl/database/differentiator.py
@@ -25,7 +25,12 @@ class Differentiator:
             is_unique_source_column = True
             for target_column in target_column_list:
                 if source_column['name'] == target_column['name']:
-                    same_name_columns.append(source_column['name'])
+                    column_dict = {
+                        "source_table": source_table,
+                        "target_table": target_table,
+                        "column": source_column['name']
+                    }
+                    same_name_columns.append(column_dict)
                 try:
                     similarity_source = source_column['data'].isin(target_column['data'])
                     similarity_target = target_column['data'].isin(source_column['data'])
@@ -33,7 +38,9 @@ class Differentiator:
                     if similarity >= similarity_threshold:
                         is_unique_source_column = False
                         column_dict = {
+                            "source_table": source_table,
                             "source_column": source_column['name'],
+                            "target_table": target_table,
                             "target_column": target_column['name'],
                             "similarity": similarity
                         }
@@ -43,34 +50,29 @@ class Differentiator:
                 except (ValueError, TypeError) as e:
                     logger.debug(f'{source_column["name"]} and {target_column["name"]} are not comparable: {e}')
             if is_unique_source_column:
-                unique_source_columns.append(source_column['name'])
+                column_dict = {
+                    "table": source_table,
+                    "column": source_column['name']
+                }
+                unique_source_columns.append(column_dict)
         unique_target_columns = []
         if non_unique_target_columns.__len__() < target_columns.__len__():
-            unique_target_columns = [column for column in target_columns if column not in non_unique_target_columns]
-        same_name_columns.sort()
-        unique_source_columns.sort()
-        unique_target_columns.sort()
-        message = (
-            f'\n{"=" * 50}\ntable comparison between {source_table} and {target_table}\n'
-            f'{"*" * 8} Columns with the same name:\n'
-            f'{", ".join(same_name_columns)}\n'
-            f'{"*" * 8} Columns with similar data:\n'
-            f'{"\n".join([f'{column}' for column in similar_columns])}\n'
-            f'{"*" * 8} Source Columns with unique data:\n'
-            f'{", ".join(unique_source_columns)}\n'
-            f'{"*" * 8} Target Columns with unique data:\n'
-            f'{", ".join(unique_target_columns)}\n'
-            f'\n{"=" * 50}\n'
-        )
-        logger.info(message)
-        return same_name_columns, similar_columns, unique_source_columns, unique_target_columns
+            unique_target_columns = [{"table": target_table, "column": column} for column in target_columns if
+                                     column not in non_unique_target_columns]
+
+        similarity_df = pd.DataFrame(source_column_list)
+        same_name_df = pd.DataFrame(same_name_columns)
+        unique_source_df = pd.DataFrame(unique_source_columns)
+        unique_target_df = pd.DataFrame(unique_target_columns)
+        unique_df = pd.concat([unique_source_df, unique_target_df])
+        return similarity_df, same_name_df, unique_df
 
     @staticmethod
     def get_column_dict_list(connection, column_names, schema, table):
         source_column_list = []
         for column in column_names:
             query = f'select distinct ([{column}]) from {schema}.{table}'
-            column_series = pd.read_sql(query, connection).squeeze()
+            column_series = pd.read_sql(query, connection)[column]
             column_series = column_series.dropna()
             column_dict = {"name": column, "data": column_series}
             source_column_list.append(column_dict)
@@ -95,46 +97,39 @@ class Differentiator:
         )
         table_info_df = pd.read_sql(get_table_info_query, connection)
         table_list = table_info_df['TABLE_NAME'].tolist()
-        same_name_columns_list = []
-        similar_columns_list = []
-        unique_columns_list = []
+        same_name_list = []
+        similarity_list = []
+        unique_list = []
         for table_set in itertools.combinations(table_list, 2):
-            same_name_columns, similar_columns, unique_source_columns, unique_target_columns = Differentiator.find_table_similarities(
+            similarity_df, same_name_df, unique_df = Differentiator.find_table_similarities(
                 connection, schema, schema, table_set[0], table_set[1], similarity_threshold)
-            for column in same_name_columns:
-                source_column = f'{table_set[0]}.{column}'
-                target_column = f'{table_set[1]}.{column}'
-                if source_column not in same_name_columns_list:
-                    same_name_columns_list.append(source_column)
-                if target_column not in same_name_columns_list:
-                    same_name_columns_list.append(target_column)
-            for column in similar_columns:
-                source_column = f'{table_set[0]}.{column["source_column"]}'
-                target_column = f'{table_set[1]}.{column["target_column"]}'
-                if source_column not in similar_columns_list:
-                    similar_columns_list.append(source_column)
-                    if source_column in unique_columns_list:
-                        unique_columns_list.remove(source_column)
-                if target_column not in similar_columns_list:
-                    similar_columns_list.append(target_column)
-                    if target_column in unique_columns_list:
-                        unique_columns_list.remove(target_column)
-            for column in unique_source_columns:
-                source_column = f'{table_set[0]}.{column}'
-                if source_column not in unique_columns_list and source_column not in similar_columns_list:
-                    unique_columns_list.append(source_column)
-        same_name_columns_list.sort()
-        similar_columns_list.sort()
-        unique_columns_list.sort()
-        message = (
-            f'\n{"=" * 50}\n{schema} schema table differences\n'
-            f'{"*" * 8} Columns with the same name:\n'
-            f'{", ".join(same_name_columns_list)}\n'
-            f'{"*" * 8} Columns with similar data:\n'
-            f'{", ".join(similar_columns_list)}\n'
-            f'{"*" * 8} Columns with unique data:\n'
-            f'{", ".join(unique_columns_list)}\n'
-            f'\n{"=" * 50}\n'
-        )
-        logger.info(message)
-        return same_name_columns_list, similar_columns_list, unique_columns_list
+            same_name_list.append(same_name_df)
+            similarity_list.append(similarity_df)
+            unique_list.append(unique_df)
+        schema_similarities_df = pd.concat(similarity_list)
+        schema_same_name_df = pd.concat(same_name_list)
+        schema_unique_df = pd.concat(unique_list)
+
+        # Combine table and column in both DataFrames for comparison
+        schema_unique_df['combined'] = schema_unique_df['table'] + '.' + schema_unique_df['column']
+        schema_similarities_df['combined_source'] = schema_similarities_df['source_table'] + '.' + \
+                                                    schema_similarities_df[
+                                                        'source_column']
+        schema_similarities_df['combined_target'] = schema_similarities_df['target_table'] + '.' + \
+                                                    schema_similarities_df[
+                                                        'target_column']
+
+        # Combine all "similar" columns into one series for exclusion
+        similar_columns_combined = pd.concat([
+            schema_similarities_df['combined_source'],
+            schema_similarities_df['combined_target']
+        ])
+
+        # Filter out rows from schema_unique_df that match any in schema_similarities
+        filtered_schema_unique_df = schema_unique_df[~schema_unique_df['combined'].isin(similar_columns_combined)]
+
+        # drop the combined column not needed anymore
+        filtered_schema_unique_df = filtered_schema_unique_df.drop(columns=['combined'])
+        schema_similarities_df = schema_similarities_df.drop(columns=['combined_source', 'combined_target'])
+
+        return schema_same_name_df, schema_similarities_df, filtered_schema_unique_df
